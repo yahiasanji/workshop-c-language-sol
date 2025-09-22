aa0e49b50523315b8b466fb395c1ef2c53864cc0
yahiasanji <yahiasanji@gmail.com>
2025-09-22 15:50:35 +0100

    challenge 6

---
+++ Day01/Variables/challenge6.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main(){
+	float a,b; 
+
+	printf("Please enter a: ");
+	scanf("%f",&a);
+	printf("Please enter b: ");
+	scanf("%f",&b); 
+	printf("a+b: %f \na-b: %f \na*b: %f \na/b: %f \n",a+b,a-b,a*b,a/b); 
+}
