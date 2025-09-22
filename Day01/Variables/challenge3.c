98b636cbffadeeb7788c14c2f878357b461ffca7
yahiasanji <yahiasanji@gmail.com>
2025-09-22 15:27:00 +0100

    challenge 3

---
+++ Day01/Variables/challenge3.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+
+int main(){
+	float Km;
+	float Y; 
+
+	printf("enter la distance en Kilometre: ");
+	scanf("%f",&Km); 
+
+	printf("\n la distance en Yards est: %.2f",Km * 1093.61); 
+}
